8a53ae28066dc71adaaaa8eaf317b98b3df32cb7
Pedro-Roque <padr@kth.se>
2025-06-09 14:23:15 +0200

    feat: add namespace to controller script

--- atmos_fmq/atmos_fmq/controller.py
+++ atmos_fmq/atmos_fmq/controller.py
@@ -11,23 +11,26 @@ from geometry_msgs.msg import PoseStamped
 class MsgHandlerController(Node):
     def __init__(self):
         super().__init__('msg_handler_robot')
-        
 
-
-        qos_profile = QoSProfile(
+        fmq_qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
             history=QoSHistoryPolicy.KEEP_LAST,
             depth=10,
         )
 
-        self.control_pub = self.create_publisher(PoseStamped, 'fmq/control', qos_profile)
-        self.state_pub = self.create_publisher(PoseStamped, 'robot_pose', qos_profile)
+        # Get namespace
+        self.namespace = self.declare_parameter('namespace', '').value
+        self.namespace_prefix = f'/{self.namespace}' if self.namespace else ''
 
-        self.setpoint_sub = self.create_subscription(PoseStamped, '/px4_mpc/setpoint_pose', self.setpoint_callback, qos_profile)
-        self.state_sub = self.create_subscription(PoseStamped, 'fmq/state', self.state_callback, qos_profile)
+        self.control_pub = self.create_publisher(PoseStamped,  f'{self.namespace_prefix}/fmq/control', fmq_qos_profile)
+        self.state_pub = self.create_publisher(PoseStamped,  f'{self.namespace_prefix}/robot_pose', 10)
+
+        self.setpoint_sub = self.create_subscription(PoseStamped,  f'{self.namespace_prefix}/px4_mpc/setpoint_pose', self.setpoint_callback, 10)
+        self.state_sub = self.create_subscription(PoseStamped,  f'{self.namespace_prefix}/fmq/state', self.state_callback, fmq_qos_profile)
 
     def setpoint_callback(self, msg: PoseStamped):
         self.control_pub.publish(msg)
+
     def state_callback(self, msg: PoseStamped):
         self.state_pub.publish(msg)
 
